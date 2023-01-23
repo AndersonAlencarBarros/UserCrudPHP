707f9916b4040cc5263b894ff8fd74613d808110
Anderson de Alencar Barros <alencarbarros@protonmail.com>
2023-01-23 13:25:38 -0300

    handle form with php

--- production/index.php
+++ production/index.php
@@ -268,10 +268,10 @@
 
           <div class="x_content">
 
-            <form class="form-label-left input_mask">
+            <form class="form-label-left input_mask" action="form.php" method="POST">
 
               <div class="col-md-12 col-sm-12  form-group has-feedback">
-                <input type="text" class="form-control has-feedback-left" id="name" name="name" placeholder="Nome Completo" />
+                <input type="text" class="form-control has-feedback-left" id="name" name="name_complete" placeholder="Nome Completo" />
                 <span class="fa fa-user form-control-feedback left" aria-hidden="true"></span>
               </div>
 
